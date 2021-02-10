629d303223451f25d07d998e8332597108f0f0bd
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-10 10:36:19 -0600

    refactored, depth-first addition

--- binary_search.rb
+++ binary_search.rb
@@ -1,5 +1,4 @@
-=begin
-
+# basic search
 def binary_search (arr, target)
   
   arr.length > 1 ? middle = (arr.length) / 2 : middle = arr[0]
@@ -13,12 +12,10 @@ def binary_search (arr, target)
 end  
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-binary_search(arr, 5)
-
-=end
+#binary_search(arr, 5)
 
 
-#tree creation / level-order traversal
+#tree creation / level-order and depth-first traversals
 class Node 
   attr_accessor :data, :left_child, :right_child
   def initialize (data, left_child, right_child)
@@ -27,33 +24,6 @@ class Node
     @right_child = right_child
   end
 end  
-
-$queue = []
-
-def enqueue (node)
-  if $queue.empty? 
-    $queue.push(node)
-  end
-  puts node.data
-  if node.left_child != nil 
-    $queue.push(node.left_child)
-  end  
-  if node.right_child != nil
-    $queue.push(node.right_child)
-  end
-  dequeue
-end  
-
-def leaf(node)
-  if node.left_child == nil && node.right_child == nil
-    enqueue($queue[0])
-  end
-end  
-
-def dequeue ()
-  $queue.shift()
-end  
-
 
 o = Node.new("O", nil, nil)
 n = Node.new("N", nil, nil)
@@ -72,24 +42,42 @@ b = Node.new("B", d, e)
 a = Node.new("A", b, c)
 
 
-def level_order_traversal (node)
-  
+def level_order_traversal(node)
   if node == nil
     return
   end  
-
-  if node.right_child == nil && node.left_child == nil
-    leaf(node)
-    level_order_traversal($queue[0])
-  else
-    enqueue(node)  
-    level_order_traversal($queue[0])
-  end  
- 
+  queue = []
+  queue.push(node)
+  while !queue.empty?
+    current_node = queue[0]
+    puts current_node.data
+    if current_node.left_child != nil
+      queue.push current_node.left_child
+    end
+    if current_node.right_child != nil
+      queue.push current_node.right_child
+    end
+    queue.shift
+  end
 end
+  
+#level_order_traversal(a)
 
-level_order_traversal(a)  
+
+def preorder_depth_first_traversal (node)
+
+  puts node.data
+
+  if node.left_child == nil && node.right_child == nil
+    return
+  end  
+
+  preorder_depth_first_traversal(node.left_child)
+  preorder_depth_first_traversal(node.right_child)
+end  
     
+#preorder_depth_first_traversal(a)
+
 
 
 
